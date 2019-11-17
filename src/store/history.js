9337bbb4211ee547c5033667d1b272771c59592a
Anthony Ngene <ngeneanthony@gmail.com>
2019-11-17 23:18:46 +0100

    remove un-used variable

--- src/store/history.js
+++ src/store/history.js
@@ -1,12 +1,10 @@
 export default {
   namespaced: true,
   state: {
-    cursor: null,
     history: []
   },
   mutations: {
     addAction(state, { word, prevState }) {
-      state.cursor = null;
       state.history.unshift({
         id: Date.now() + Math.floor(Math.random() * 1000).toString(),
         word,
