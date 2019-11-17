b536dd8ca041f9b2f073a06416ba077911c970b9
Anthony Ngene <ngeneanthony@gmail.com>
2019-11-17 18:46:59 +0100

    implement movement animation

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -1,16 +1,18 @@
 <template>
   <section id="posts">
     <ul>
-      <template v-for="(post, index) in posts">
-        <Post
-          :key="post.id"
-          :post="post"
-          :index="index"
-          :moveUp="moveUp"
-          :moveDown="moveDown"
-          :isLastPost="index == posts.length - 1 ? true : false"
-        />
-      </template>
+      <transition-group name="slide">
+        <template v-for="(post, index) in posts">
+          <Post
+            :key="post.id"
+            :post="post"
+            :index="index"
+            :moveUp="moveUp"
+            :moveDown="moveDown"
+            :isLastPost="index == posts.length - 1 ? true : false"
+          />
+        </template>
+      </transition-group>
     </ul>
   </section>
 </template>
@@ -22,12 +24,9 @@ export default {
   name: "posts",
   components: { Post },
   data() {
-    return {};
-  },
-  computed: {
-    posts() {
-      return this.$store.state.posts.posts;
-    }
+    return {
+      posts: []
+    };
   },
   mounted() {
     this.getPosts();
@@ -36,13 +35,23 @@ export default {
     async getPosts() {
       if (this.posts.length == 0) {
         await this.$store.dispatch("posts/getPosts");
+        this.posts = this.$store.state.posts.posts;
       }
     },
     moveUp(idx) {
-      this.$store.commit("posts/moveUp", idx);
+      if (idx > 0 && idx <= this.posts.length) {
+        this.swap(this.posts, idx, idx - 1);
+      }
     },
     moveDown(idx) {
-      this.$store.commit("posts/moveDown", idx);
+      if (idx < this.posts.length - 1 && idx >= 0) {
+        this.swap(this.posts, idx, idx + 1);
+      }
+    },
+    swap(arr, x, y) {
+      const temp = arr[x];
+      arr.splice(x, 1, arr[y]);
+      arr.splice(y, 1, temp);
     }
   }
 };
@@ -55,5 +64,9 @@ li {
   border: 1px solid gray;
   margin: 5px 0;
   width: 500px;
+}
+
+.slide-move {
+  transition: transform 0.5s cubic-bezier(0.55, 0, 0.1, 1);
 }
 </style>
--- src/store/posts.js
+++ src/store/posts.js
@@ -16,24 +16,6 @@ export default {
   mutations: {
     getPostsSuccess(state, posts) {
       state.posts = posts.slice(0, 5);
-    },
-    moveUp(state, idx) {
-      if (idx > 0 && idx <= state.posts.length) {
-        const posts = state.posts;
-        swap(posts, idx, idx - 1);
-        state.posts = [...posts];
-      }
-    },
-    moveDown(state, idx) {
-      if (idx < state.posts.length - 1 && idx >= 0) {
-        const posts = state.posts;
-        swap(posts, idx, idx + 1);
-        state.posts = [...posts];
-      }
     }
   }
 };
-
-function swap(arr, x, y) {
-  [arr[x], arr[y]] = [arr[y], arr[x]];
-}
